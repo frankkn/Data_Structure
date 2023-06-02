5bcf531a45607a90d4c7faa441ffacb2ccd1ebee
Frank <frank840629@gmail.com>
2023-06-02 19:59:17 +0800

    Need to debug

---
+++ 9-1 Adding edges/0602.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class DisjointSet 
+{
+  vector<int> parent;
+  int find_root(int x) 
+  {
+    if (x == parent[x]) return x;
+    int root = find_root(parent[x]);
+    return parent[x] = root; // 路徑壓縮
+  }
+
+public:
+  DisjointSet(int n):parent(n + 1) { init(); }
+  void init() 
+  {
+    for (int i = 0; i < parent.size(); ++i)
+      parent[i] = i;
+  }
+  bool Same(int a, int b) { return find_root(a) == find_root(b); }
+  void Union(int a, int b) { parent[find_root(a)] = find_root(b); }
+};
+
+int main(){
+  int n, m;
+	cin >> n >> m;
+  DisjointSet DS(n);
+  int cnt = n, maxcc = 1;
+  vector<int> component_size(n+1, 1);
+  for(int i = 0; i < m; ++i){
+    int x, y; cin >> x >> y;
+    if(!DS.Same(x, y)){
+      int new_size = component_size[x] + component_size[y];
+      component_size[x] = component_size[y] = new_size;
+      DS.Union(x, y);
+      cnt--;
+      maxcc = max(maxcc, new_size);
+    }
+    cout << cnt << " " << maxcc << "\n";
+  }
+	return 0;
+}
